c2312d545f9ed47d85f1405ce70c5549f5223527
Yulia Yamalieva <yulia.yamalieva@yandex.ru>
2023-08-20 17:12:42 +0300

    Сверстан блок footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,41 @@
+import './Footer.css';
+
 function Footer() {
-  console.log('Hello');
+  return (
+    <footer className="footer">
+      <h3 className="footer__title">
+        Учебный проект Яндекс.Практикум х BeatFilm.
+      </h3>
+      <div className="footer__container">
+        <p className="footer__year">&copy; 2020</p>
+
+        <nav className="footer__nav">
+          <ul className="footer__list">
+            <li className="footer__item">
+              <a
+                className="footer__link"
+                href="https://practicum.yandex.ru/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Яндекс.Практикум
+              </a>
+            </li>
+            <li className="footer__item">
+              <a
+                className="footer__link"
+                href="https://github.com/yulieyv"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Github
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
